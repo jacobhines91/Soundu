7081469d856b51097a0d68e892ceb14835c3efa0
Lisa Smithen <smithenlisa@gmail.com>
2019-09-07 13:04:31 -0500

    Pseudo code updated. All teammembers are to review and make suggestions either by slack or in code

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -9,16 +9,20 @@
 
 //A click function where the user enters the name of a song and the function performs the following
 //  1. pushes the name of the song into the database
-//  2. pulls from Music Graph API the corresponding information
+//  2. pulls from Spotify API the corresponding information
 //  3. pulls from Youtube API the corresponding video
+//  4. pulls suggested playlist or suggested videos from Youtube API or Spotify
 
 
 //songVideoDisplay that pulls up must popular video related to the song pulled from You Tube API
 
 
-//songInfoDisplay pulls up the following items from the Music Graph API
-//Items to be displayed: 1. Name of the Artist
+//songInfoDisplay pulls up the following items from the Spotify API
+            //Items to be displayed: 1. Name of the Artist 2. Release Date 3. Popularity 4. Genre 5. Album Type
 
 //buttonViewDisplay shows the last ten searches that was entered by users
+
+//SuggestionPlaylist/Video from the Spotify or Youtube API
+            //Based on the song name entered by the user this will display either the name of four songs they shoud try or four videos they should look at
 
 
