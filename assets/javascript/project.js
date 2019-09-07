efb9fc997b799da02c4ac028b5806cbd56846ea2
Lisa Smithen <smithenlisa@gmail.com>
2019-09-07 12:52:38 -0500

    Html skeleton added using bootstrap. Psuedo code added to javascript

---
+++ assets/javascript/project.js
@@ -0,0 +1,24 @@
+//Subscibersbox
+//      It will collect the information and push it to Firebase
+
+
+//A button that shows the updated number of subscribers that ties into Firebase:
+//      This information is pulled from Firebase
+
+
+
+//A click function where the user enters the name of a song and the function performs the following
+//  1. pushes the name of the song into the database
+//  2. pulls from Music Graph API the corresponding information
+//  3. pulls from Youtube API the corresponding video
+
+
+//songVideoDisplay that pulls up must popular video related to the song pulled from You Tube API
+
+
+//songInfoDisplay pulls up the following items from the Music Graph API
+//Items to be displayed: 1. Name of the Artist
+
+//buttonViewDisplay shows the last ten searches that was entered by users
+
+
