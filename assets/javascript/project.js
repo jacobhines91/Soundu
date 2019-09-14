30322da45cf9a7568c970c10b8a6fc52ae3154b1
Keisha <kmmatthews15@gmail.com>
2019-09-14 09:08:22 -0500

    added the youtube video embed code

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -15,6 +15,46 @@
   function HideDialog() {
     $("#bkgOverlay").fadeOut(400);
     $("#delayedPopup").fadeOut(300);
+  } 
+
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '720',
+      videoId: 'M7lc1UVf-VE',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    // if (event.data == YT.PlayerState.PLAYING && !done) {
+    //   setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  
+  function stopVideo() {
+    player.stopVideo();
   }
 
  var subscribersBox;
@@ -68,6 +108,7 @@ database.ref().on("value", function(snapshot) {
 
 
 
+
 //A click function where the user enters the name of a song and the function performs the following
 //  1. pushes the name of the song into the database
 //  2. pulls from Spotify API the corresponding information
@@ -102,7 +143,8 @@ $("#searchButton").on("click", callerFunction)
 function callerFunction( )
 {
 	//call function which has YouTube logic
-	youtubeLogic( 
+	youtubeLogic(  
+    
         
     );
 
